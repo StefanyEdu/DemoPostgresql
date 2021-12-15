2a3bc537da136f6965ae33260d7a37d1bb674691
Caroline Moraes <carol.2014pess@gmail.com>
2021-12-15 17:13:22 -0300

    Correction of the cartaUnica method.

--- src/main/java/com/example/db/coracoesDeLila/resouces/ConsultaDasCartas.java
+++ src/main/java/com/example/db/coracoesDeLila/resouces/ConsultaDasCartas.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,21 +26,14 @@ public class ConsultaDasCartas {
 
 	}
 
-	@GetMapping("/cartas")
-	public List<Cartas> listaCartasUnico() {
-		return cartasRepository.findAll();
+	//busca por um unico produto
+	@GetMapping("/cartas/{id}") 
+	public Cartas cartaUnica(@PathVariable(value = "id") long id) { 
+		return cartasRepository.findById(id);
+	 
+	 }
 
-	}
-// busca por um unico produto
-
-	/*
-	 * @GetMapping("/cartas/{id}") public Cartas CartasUnico(@PathVariable(value =
-	 * "id") long id{ return cartasRepository.findById(id);
-	 * 
-	 * }
-	 */
-
-//salvar
+	//salvar
 	@PostMapping("/Cartas")
 	public Cartas salvaCartas(@RequestBody Cartas cartas) {
 		return cartasRepository.save(cartas);
